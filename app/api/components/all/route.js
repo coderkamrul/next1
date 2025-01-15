dff81761fe0fdd6e815f950a26a5f6385b4fd17f
Kamrul Hasan <kamrulhasan13020@gmail.com>
2025-01-15 23:10:18 +0600

    update 3.0.13

--- app/api/components/all/route.js
+++ app/api/components/all/route.js
@@ -1,18 +1,10 @@
-import { NextRequest, NextResponse } from 'next/server'
-import { getServerSession } from 'next-auth/next'
+import { NextResponse } from 'next/server'
+i
 import dbConnect from '@/lib/mongodb'
 import Components from '@/models/Components'
-import { authOptions } from '@/app/api/auth/[...nextauth]/route'
 
 export async function GET(req) {
   await dbConnect()
-  const session = await getServerSession(authOptions)
-  if (!session) {
-    return NextResponse.json(
-      { message: 'You must be signed in to access this route.' },
-      { status: 401 }
-    )
-  }
 
   const components = await Components.find().populate('versions').lean()
 
